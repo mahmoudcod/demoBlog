0c1b79145d65f1ed417fe1e5470a7e5b84b6f200
mahmoudcod <mahmoud.pk@icloud.com>
2024-04-21 02:40:18 +0200

    try to make the graph ql to work in production

--- config/plugins.js
+++ config/plugins.js
@@ -15,5 +15,19 @@ module.exports = ({ env }) => ({
       },
     },
   },
+
+  graphql: {
+    enabled: true,
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: true,
+      defaultLimit: 10,
+      maxLimit: 20,
+      apolloServer: {
+        tracing: true,
+      },
+    },
+  },
   // ...
 });
